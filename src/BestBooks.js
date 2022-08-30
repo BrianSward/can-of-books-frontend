eda9a21e78199cec3ec2ad0ca0cae0f7cd60493a
Xavier Hillman <xhillman13@gmail.com>
2022-08-30 18:09:45 -0400

    add form modal to add new books to database

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -2,12 +2,14 @@ import React from 'react';
 import axios from 'axios';
 import Carousel from 'react-bootstrap/Carousel';
 import photo from './img/photo.jpg';
+import BookFormModal from './BookFormModal';
 
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       books: [],
+      showModal: false,
     }
   }
   
@@ -22,6 +24,36 @@ class BestBooks extends React.Component {
     } catch (error) {
       console.log('we have an error: ', error.response);
     }
+  }
+
+  openPopUp = () => {
+    this.setState({showModal: true});
+  }
+
+  closePopUp = () => {
+    this.setState({showModal: false});
+  }
+
+  createNewBook = async (bookInfo) => {
+    console.log(bookInfo);
+    try {
+      let response = await axios.post(`${process.env.REACT_APP_SERVER}/books`, bookInfo);
+      let newBook = response.data;
+      this.setState({
+        books: [...this.state.books, newBook],
+      });
+    } catch (error) {
+      console.log('error posting book ', error.response);
+    }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.createNewBook({
+      title: event.target.formTitle.value,
+      description: event.target.formDescription.value,
+      status: event.target.formStatus.checked,
+    })
   }
   
   componentDidMount() {
@@ -58,6 +90,9 @@ class BestBooks extends React.Component {
           ) : (
             <h3>No Books Found :(</h3>
             )}
+        <BookFormModal handleSubmit={this.handleSubmit} show={this.state.showModal} open={this.openPopUp}
+                       close={this.closePopUp}/>
+        <button onClick={this.openPopUp}>Add Book</button>
       </>
     )
   }
